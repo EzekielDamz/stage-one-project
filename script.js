78d0b111128189159a71e71257e077d65759c7fb
damilare dolapo jimoh <ezekieldammy1@gmail.com>
2023-09-09 23:23:29 +0100

    updated HNG

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const d = new Date();
 let day = weekday[d.getDay()];
-document.getElementById("slackDayOfTheWeek").innerHTML = "Current Day: "+ day;
+document.querySelector('[data-testid=slackDayOfTheWeek]').innerHTML = "Current Day: " + day;
+const dayTime = document.querySelector('[data-testid=currentUTCTime]');
 
-const t = new Date(); 
-document.getElementById("currentUTCTime").innerHTML = "Current Time: " + t.toUTCString();
- 
+const UTCTime = () => {
+      
+    const getUTCTiming = () => {
+        const Newtime = new Date()
+        const hour = Newtime.getUTCHours()
+        const minutes = Newtime.getMinutes()
+        const seconds = Newtime.getSeconds()
+        /*const milliseconds = Newtime.getMilliseconds()*/
+        const currentUTCTiming = `Current Time: ${hour}: ${minutes}: ${seconds}`
+        dayTime.innerHTML = currentUTCTiming;
+     }
+     getUTCTiming()
+
+     setInterval(getUTCTiming, 1000)
+}
+UTCTime()
+
+  
