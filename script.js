7520eb7776069946ad14dbf96a7037374b4793dd
damilare dolapo jimoh <ezekieldammy1@gmail.com>
2023-09-10 20:40:57 +0100

    Final Change

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 const d = new Date();
+
 let day = weekday[d.getDay()];
 document.querySelector('[data-testid=slackDayOfTheWeek]').innerHTML = "Current Day: " + day;
 
-
 const dayTime = document.querySelector('[data-testid=currentUTCTime]');
 
-
-
-const UTCTime = () => {
-      
-    const getUTCTiming = () => {
-        const Newtime = new Date()
-        const hour = Newtime.getUTCHours()
-        const minutes = Newtime.getMinutes()
-        const seconds = Newtime.getSeconds()
-        const milliseconds = Newtime.getMilliseconds()
-        const currentUTCTiming = `Current Time: ${hour}: ${minutes}: ${seconds}: ${milliseconds}`
-        dayTime.innerHTML = currentUTCTiming;
-     }
-     getUTCTiming()
-
-     setInterval(getUTCTiming, 1000)
-}
-UTCTime()
-
-  
+const currentUTCTiming = d.toUTCString()
+dayTime.innerHTML = currentUTCTiming;
